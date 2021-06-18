71831516cb0180d6fe70832f0fd309f5f1fba5ba
Rodney Santiago <GD000007711@ups.com>
2021-06-18 17:18:13 -0400

    Added a point graphic and updated notes of article.

--- DisplayMap/MapViewModel.cs
+++ DisplayMap/MapViewModel.cs
@@ -2,6 +2,8 @@
 using Esri.ArcGISRuntime.Mapping;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.UI;
 
 namespace DisplayMap
 {
@@ -11,6 +13,8 @@ namespace DisplayMap
         public MapViewModel()
         {
             SetupMap();
+
+            CreateGraphics();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -30,12 +34,67 @@ namespace DisplayMap
             }
         }
 
+        private GraphicsOverlayCollection _graphicsOverlays;
+        public GraphicsOverlayCollection GraphicsOverlays
+        {
+            get { return _graphicsOverlays; }
+            set
+            {
+                _graphicsOverlays = value;
+                OnPropertyChanged();
+            }
+        }
+
         private void SetupMap()
         {
 
             // Create a new map with a 'topographic vector' basemap.
             Map = new Map(BasemapStyle.ArcGISTopographic);
 
+        }
+
+        private void CreateGraphics()
+        {
+            // Create a new graphics overlay to contain a variety of graphics.
+            var malibuGraphicsOverlay = new GraphicsOverlay();
+
+            // Add the overlay to a graphics overlay collection.
+            GraphicsOverlayCollection overlays = new GraphicsOverlayCollection
+            {
+                malibuGraphicsOverlay
+            };
+
+            // Set the view model's "GraphicsOverlays" property (will be consumed by the map view).
+            this.GraphicsOverlays = overlays;
+
+            //######################################
+
+            // Create a point geometry.
+            var dumeBeachPoint = new MapPoint(-118.8066, 34.0006, SpatialReferences.Wgs84);
+
+            // Create a symbol to define how the point is displayed.
+            var pointSymbol = new SimpleMarkerSymbol
+            {
+                Style = SimpleMarkerSymbolStyle.Circle,
+                Color = System.Drawing.Color.Orange,
+                Size = 10.0
+            };
+
+            // Add an outline to the symbol.
+            pointSymbol.Outline = new SimpleLineSymbol
+            {
+                Style = SimpleLineSymbolStyle.Solid,
+                Color = System.Drawing.Color.Blue,
+                Width = 2.0
+            };
+
+            //######################################
+
+            // Create a point graphic with the geometry and symbol.
+            var pointGraphic = new Graphic(dumeBeachPoint, pointSymbol);
+
+            // Add the point graphic to graphics overlay.
+            malibuGraphicsOverlay.Graphics.Add(pointGraphic);
         }
     }
 }
--- DisplayMap/Readme.md
+++ DisplayMap/Readme.md
@@ -1,4 +1,9 @@
 ﻿# ArcGIS Practice  
 
 6/18/21  
-Tutiorial [here](https://developers.arcgis.com/net/maps-2d/tutorials/display-a-map/)  
+  
+Display a map tutiorial [here](https://developers.arcgis.com/net/maps-2d/tutorials/display-a-map/)  
+
+Add a point, line, and polygon [here](https://developers.arcgis.com/net/maps-2d/tutorials/add-a-point-line-and-polygon/)  
+
+
