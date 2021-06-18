e8e12a7fa328acde3db86f079c34753d06dec667
Rodney Santiago <rodsan.724@outlook.com>
2021-06-18 14:16:21 -0400

    wont run thinking it might have something to do with running in Parallels, perhaps.

--- DisplayMap/MainWindow.xaml.cs
+++ DisplayMap/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DisplayMap
 {
@@ -23,6 +12,10 @@ namespace DisplayMap
         public MainWindow()
         {
             InitializeComponent();
+
+            MapPoint mapCenterPoint = new MapPoint(-118.805, 34.027, SpatialReferences.Wgs84);
+            MainMapView.SetViewpoint(new Viewpoint(mapCenterPoint, 100000));
+
         }
     }
 }
---
+++ DisplayMap/MapViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DisplayMap
+{
+    class MapViewModel : INotifyPropertyChanged
+    {
+
+        public MapViewModel()
+        {
+            SetupMap();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Map _map;
+        public Map Map
+        {
+            get { return _map; }
+            set
+            {
+                _map = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void SetupMap()
+        {
+
+            // Create a new map with a 'topographic vector' basemap.
+            Map = new Map(BasemapStyle.ArcGISTopographic);
+
+        }
+    }
+}
